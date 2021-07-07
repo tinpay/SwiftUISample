9fac2ef492de21240f1577842157408e27e07c45
tinpay <sfukui@gmail.com>
2021-07-07 09:21:12 +0900

    よくわかっていないコードをコメントアウト

--- Shared/CustomWebView.swift
+++ Shared/CustomWebView.swift
@@ -12,24 +12,23 @@ import WebKit
 struct CustomWebView: UIViewRepresentable {
     
     let url: URL
-    private let observable = WebViewURLObservable()
-
-    var observer: NSKeyValueObservation? {
-        observable.instance
-    }
+//    private let observable = WebViewURLObservable()
+//
+//    var observer: NSKeyValueObservation? {
+//        observable.instance
+//    }
 
     func makeUIView(context: Context) -> WKWebView {
         WKWebView()
     }
 
     func updateUIView(_ uiView: WKWebView, context: Context) {
-        observable.instance = uiView.observe(\WKWebView.url, options: .new) { view, change in
-            print("Page URL: \(url)")
-        }
+//        observable.instance = uiView.observe(\WKWebView.url, options: .new) { view, change in
+//        }
         uiView.load(URLRequest(url: url))
     }
 }
 
-private class WebViewURLObservable: ObservableObject {
-    @Published var instance: NSKeyValueObservation?
-}
+//private class WebViewURLObservable: ObservableObject {
+//    @Published var instance: NSKeyValueObservation?
+//}
